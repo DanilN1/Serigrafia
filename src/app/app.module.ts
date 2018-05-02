42e670efa5922f5ef65caffd6edf1280b4c8edd7
Ramon Ontiveros <rontiveros@sitek.mx>
2018-05-01 19:51:23 -0600

    Separacion de html en componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,31 @@ import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { ToolboxComponent } from './toolbox/toolbox.component';
+import { ItemsComponent } from './items/items.component';
+import { PerspectivesComponent } from './perspectives/perspectives.component';
+import { PalletComponent } from './pallet/pallet.component';
+import { CanvasComponent } from './canvas/canvas.component';
+import { BudgetComponent } from './budget/budget.component';
+import { BudgetDetailComponent } from './budget-detail/budget-detail.component';
 
 const appRoutes: Routes = [
-  /*{ path: 'foo', component: FooComponent },
-  { path: '', redirectTo: '/foo', pathMatch: 'full' },*/
-  { path: '**', component: PageNotFoundComponent }
+    { path: '', component: BudgetComponent },
+    { path: '**', component: PageNotFoundComponent }
 ];
 
 
 @NgModule({
     declarations: [
 	AppComponent,
-	PageNotFoundComponent
+	PageNotFoundComponent,
+	ToolboxComponent,
+	ItemsComponent,
+	PerspectivesComponent,
+	PalletComponent,
+	CanvasComponent,
+	BudgetComponent,
+	BudgetDetailComponent
     ],
     imports: [
 	BrowserModule,
